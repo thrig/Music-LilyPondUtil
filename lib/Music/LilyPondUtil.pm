102110156ad8160a1fb05a7ed6078f7d055f4f98
Jeremy Mates <jmates@cpan.org>
2012-10-25 01:24:05 +0000

    Initial work.

---
+++ lib/Music/LilyPondUtil.pm
@@ -0,0 +1,44 @@
+# http://www.lilypond.org/
+
+package Music::LilyPondUtil;
+
+use 5.010000;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=head1 NAME
+
+Music::LilyPondUtil - utility methods for lilypond data
+
+=head1 SYNOPSIS
+
+  use Music::LilyPondUtil;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+TODO
+
+=head1 SEE ALSO
+
+http://www.lilypond.org/
+
+=head1 AUTHOR
+
+Jeremy Mates, E<lt>jmates@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Jeremy Mates
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself, either Perl version 5.16.1 or, at
+your option, any later version of Perl 5 you may have available.
+
+=cut
